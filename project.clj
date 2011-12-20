bcbcd6c354fa21e837731922c7f5b38398679167
Roman Gonzalez <romanandreg@gmail.com>
2011-12-20 08:53:07 -0800

    Changing the name of the project, and adding monads as a dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject clj-iteratees "1.0.0-SNAPSHOT"
+(defproject clj-streams "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/algo.monads "0.1.0"]])
