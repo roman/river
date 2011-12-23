f516794c8367fdcffb50fee4797c0af443ff455b
Roman Gonzalez <romanandreg@gmail.com>
2011-12-23 14:31:21 -0800

    Renaming the project to stream instead of streams

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-streams "1.0.0-SNAPSHOT"
+(defproject clj-stream "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/algo.monads "0.1.0"]])
