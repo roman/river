8a7845f07ed1780654d87c992b798da07fe3a4c5
Roman Gonzalez <romanandreg@gmail.com>
2011-12-27 16:47:49 -0800

    Adding test.generative to the dev-dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clj-stream "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
+  :dev-dependencies [[org.clojure/test.generative "0.1.3"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/algo.monads "0.1.0"]])
