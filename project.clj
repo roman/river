2c862771e683c21437616fa9ad2e1f236dc0df2a
Roman Gonzalez <romanandreg@gmail.com>
2011-12-28 23:56:44 -0800

    Changing specs on the project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-stream "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clj-stream "0.0.1-SNAPSHOT"
+  :description "Oleg's Iteratee implementation in Clojure."
   :dev-dependencies [[org.clojure/test.generative "0.1.3"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/algo.monads "0.1.0"]])
