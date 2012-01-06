3d93a27ed733cab9a6f587bbc886b36d92c12774
Roman Gonzalez <romanandreg@gmail.com>
2012-01-05 21:22:12 -0800

    Adding produce-input-stream-chars producers family

--- src/core/stream/io.clj
+++ src/core/stream/io.clj
@@ -7,6 +7,7 @@
 ;; Standard Lib ;;;;
 
   (:import [java.io LineNumberReader
+                    BufferedReader
                     InputStreamReader
                     FileInputStream])
 
@@ -21,17 +22,36 @@
 (defn produce-input-stream-bytes
   "Stream bytes from a given input-stream, and streams it to the
   given consumer. When buffer-size is given, each chunk will have
-  buffer-size number of bytes."
+  buffer-size number of bytes (defaults to 1024)."
   ([input-stream consumer]
     (produce-input-stream-bytes 1024 input-stream consumer))
 
   ([buffer-size input-stream consumer0]
+   (io!
     (let [buffer (byte-array buffer-size)]
       (loop [consumer consumer0]
         (let [n-bytes (.read input-stream buffer)]
           (if (and (>= n-bytes 0) (continue? consumer))
             (recur (consumer (take n-bytes (vec buffer))))
-            consumer))))))
+            consumer)))))))
+
+(defn produce-input-stream-chars
+  "Stream characters from a given input-stream, and streams it to the
+  given consumer. When buffer-size is given, each chunk will have
+  buffer-size number of characters (defaults to 1024)."
+  ([input-stream consumer]
+   (produce-input-stream-chars 1024 input-stream consumer))
+
+  ([buffer-size input-stream consumer0]
+    (io!
+      (let [reader (BufferedReader. 
+                     (InputStreamReader. input-stream))
+            buffer (char-array buffer-size)]
+        (loop [consumer consumer0]
+          (let [n-chars (.read reader buffer)]
+            (if (and (>= n-chars 0) (continue? consumer))
+              (recur (consumer (take n-chars (vec buffer))))
+              consumer)))))))
 
 (defn produce-input-stream-lines
   "Stream lines from a given input-stream, and streams it to the
@@ -40,7 +60,8 @@
   [input-stream consumer]
   (let [reader (LineNumberReader.
                 (InputStreamReader. input-stream))]
-    (produce-generate #(.readLine reader) consumer)))
+    (io!
+      (produce-generate #(.readLine reader) consumer))))
 
 (defn produce-file-bytes
   "Stream bytes from a file, specified by file-name, uses
@@ -48,6 +69,13 @@
   [file-name consumer]
   (with-open [input-stream (FileInputStream. file-name)]
     (produce-input-stream-bytes input-stream consumer)))
+
+(defn produce-file-chars
+  "Stream characters from a file, specified by file-name, uses
+  produce-input-stream-chars internally with a FileInputStream class."
+  [file-name consumer]
+  (with-open [input-stream (FileInputStream. file-name)]
+    (produce-input-stream-chars input-stream consumer)))
 
 (defn produce-file-lines
   "Stream lines from a file, specified by file-name, uses
@@ -64,6 +92,15 @@
                                (.exec cmd)
                                (.getInputStream))]
     (produce-input-stream-bytes input-stream consumer)))
+
+(defn produce-proc-chars
+  "Stream chars from an OS process executing the cmd command, it uses
+  produce-input-stream-chars internally."
+  [cmd consumer]
+  (with-open [input-stream (-> (Runtime/getRuntime)
+                               (.exec cmd)
+                               (.getInputStream))]
+    (produce-input-stream-chars input-stream consumer)))
 
 (defn produce-proc-lines
   "Stream lines from an OS process executing the cmd command, it uses
