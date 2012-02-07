35b284921e0256fa8783aa7210f6c697ea8a7174
Roman Gonzalez <romanandreg@gmail.com>
2012-02-07 10:30:01 -0800

    Upgrading org.clojure/algo.monads to it's latest version

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject river "0.0.1-SNAPSHOT"
-  :description "Iteratee library for Clojure."
+  :description "A monadic stream library in Clojure (port of Haskell's enumerator)"
   :author "Roman Gonzalez"
+  :repositories { "sonatype" {:url "https://oss.sonatype.org/content/repositories/snapshots/"}}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/algo.monads "0.1.0"]])
+                 [org.clojure/algo.monads "0.1.3-20120206.105742-1"]])
