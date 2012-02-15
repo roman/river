c6d0722c3cc9056b30dd5f0a187cc65859a23baa
Roman Gonzalez <romanandreg@gmail.com>
2012-02-15 00:37:24 -0800

    Bumping the current version to 0.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject river "0.0.1-SNAPSHOT"
+(defproject org.van-clj/river "0.0.2-SNAPSHOT"
   :description "A monadic stream library in Clojure (port of Haskell's enumerator)"
   :author "Roman Gonzalez"
   :repositories { "sonatype" {:url "https://oss.sonatype.org/content/repositories/snapshots/"}}
