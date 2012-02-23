9fca5a71d06f7655088163db59222fa5926b8c0f
Roman Gonzalez <romanandreg@gmail.com>
2012-02-22 23:23:43 -0800

    Enhancing the IO API for river
    * producers now have a hash option for setting encoding
    * new consumers to write strings and bytes on output-streams and
      writers

--- src/river/io.clj
+++ src/river/io.clj
@@ -18,101 +18,151 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn- io-default-options [args]
+  (->> (cond
+         (map? (first args)) args
+         :else (apply hash-map args))
+       (merge {:buffer-size 1024
+               :encoding "UTF-8"})))
+
 (defn produce-reader-chars
   "Stream characters from an input (uses clojure.java.io/reader
   internally), and streams it to the given consumer. When
   buffer-size is given, each chunk will have a buffer-size number of
   chars (defaults to 1024 chars)."
-  ([input]
-    (produce-reader-chars 1024 input))
-  ([buffer-size input]
-    (fn producer [consumer0]
-      (io!
-        (let [reader (io/reader input)
-              buffer (char-array buffer-size)]
-        (loop [consumer consumer0]
-          (let [n-chars (.read reader buffer)]
-          (if (and (>= n-chars 0) (continue? consumer))
-            (recur (consumer (take n-chars (vec buffer))))
-            consumer))))))))
+  ([input & opts]
+    (let [{:keys [buffer-size encoding]} (io-default-options opts)]
+      (fn producer [consumer0]
+        (io!
+          (let [reader (io/reader input :encoding encoding)
+                buffer (char-array buffer-size)]
+          (loop [consumer consumer0]
+            (let [n-chars (.read reader buffer)]
+            (if (and (>= n-chars 0) (continue? consumer))
+              (recur (consumer (take n-chars (vec buffer))))
+              consumer)))))))))
 
 (defn produce-reader-lines
   "Stream lines from an input element (uses clojure.java.io/reader
   internally), and streams it to the given consumer."
-  ([input]
+  ([input & opts]
     (io!
-      (let [reader (LineNumberReader. (io/reader input))]
+      (let [{:keys [encoding]} (io-default-options opts)
+            reader (LineNumberReader. (io/reader input
+                                                 :encoding encoding))]
       (produce-generate #(.readLine reader))))))
 
 (defn produce-input-stream-bytes
   "Stream bytes from a given input, and streams it to the
   given consumer. When buffer-size is given, each chunk will have
   buffer-size number of bytes (defaults to 1024)."
-  ([input]
-    (produce-input-stream-bytes 1024 input))
-
-  ([buffer-size input]
+  ([input & opts]
+    (let [{:keys [buffer-size encoding]} (io-default-options opts)]
     (fn [consumer0]
       (io!
-       (let [input-stream (io/input-stream input)
+       (let [input-stream (io/input-stream input :encoding encoding)
              buffer       (byte-array buffer-size)]
          (loop [consumer consumer0]
            (let [n-bytes (.read input-stream buffer)]
              (if (and (>= n-bytes 0) (continue? consumer))
                (recur (consumer (take n-bytes (vec buffer))))
-               consumer))))))))
+               consumer)))))))))
 
 (defn produce-file-bytes
-  "Stream bytes from a file, specified by file-name, uses
+  "Stream bytes from a file, specified by filename, uses
   produce-input-stream-bytes internally with a FileInputStream class."
-  [file-name]
-  (fn producer [consumer]
-    (with-open [input-stream (FileInputStream. file-name)]
-      ((produce-input-stream-bytes input-stream) consumer))))
+  ([filename & opts]
+   (fn producer [consumer]
+     (with-open [input-stream (FileInputStream. filename)]
+       ((apply produce-input-stream-bytes input-stream opts) consumer)))))
 
 (defn produce-file-chars
-  "Stream characters from a file, specified by file-name, uses
+  "Stream characters from a file, specified by filename, uses
   produce-input-stream-chars internally with a FileInputStream class."
-  [file-name]
-  (fn producer [consumer]
-    (with-open [input-stream (FileInputStream. file-name)]
-      ((produce-reader-chars input-stream ) consumer))))
+  ([filename & opts]
+    (fn producer [consumer]
+      (with-open [input-stream (FileInputStream. filename)]
+        ((apply produce-reader-chars input-stream opts) consumer)))))
 
 (defn produce-file-lines
-  "Stream lines from a file, specified by file-name, uses
+  "Stream lines from a file, specified by filename, uses
   produce-input-stream-lines internally with a FileInputStream class."
-  [file-name]
+  [filename & opts]
   (fn producer [consumer]
-    (with-open [input-stream (FileInputStream. file-name)]
-      ((produce-reader-lines input-stream) consumer))))
+    (with-open [input-stream (FileInputStream. filename)]
+      ((apply produce-reader-lines input-stream opts) consumer))))
 
 (defn produce-proc-bytes
   "Stream bytes from an OS process executing the cmd command, it uses
   produce-input-stream-bytes internally."
-  [cmd]
-  (fn producer [consumer]
-    (with-open [input-stream (-> (Runtime/getRuntime)
-                                 (.exec cmd)
-                                 (.getInputStream))]
-      ((produce-input-stream-bytes input-stream) consumer))))
+  ([cmd & opts]
+    (fn producer [consumer]
+      (with-open [input-stream (-> (Runtime/getRuntime)
+                                   (.exec cmd)
+                                   (.getInputStream))]
+        ((apply produce-input-stream-bytes input-stream opts) consumer)))))
 
 (defn produce-proc-chars
   "Stream chars from an OS process executing the cmd command, it uses
   produce-input-stream-chars internally."
-  [cmd]
-  (fn producer [consumer]
-  (with-open [input-stream (-> (Runtime/getRuntime)
-                               (.exec cmd)
-                               (.getInputStream))]
-    ((produce-reader-chars input-stream) consumer))))
+  ([cmd & opts]
+    (fn producer [consumer]
+    (with-open [input-stream (-> (Runtime/getRuntime)
+                                 (.exec cmd)
+                                 (.getInputStream))]
+      ((apply produce-reader-chars input-stream opts) consumer)))))
 
 (defn produce-proc-lines
   "Stream lines from an OS process executing the cmd command, it uses
   produce-input-stream-lines internally."
-  [cmd]
+  [cmd & opts]
   (fn producer [consumer]
     (with-open [input-stream (-> (Runtime/getRuntime)
                                  (.exec cmd)
                                  (.getInputStream))]
-      ((produce-reader-lines input-stream ) consumer))))
+      ((apply produce-reader-lines input-stream opts) consumer))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Consumers
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn write-bytes-to-output-stream
+  "Receives an OutputStream jk"
+  [^java.io.OutputStream output-stream]
+  (letfn [
+    (consumer [os stream]
+      (cond
+      (eof? stream)
+      (do
+        (.close os)
+        (yield nil stream))
+
+      (empty-chunk? stream)
+      (continue #(consumer os %))
+
+      :else
+      (do
+        (.write os (byte-array stream))
+        (continue #(consumer os %)))))]
+  (continue #(consumer output-stream %))))
+
+(defn write-chars-to-writer
+  [^java.io.Writer writer]
+  (letfn [
+    (consumer [wr stream]
+      (cond
+      (eof? stream)
+      (yield nil stream)
+
+      (empty? stream)
+      (continue #(consumer wr %))
+
+      :else
+      (do
+        (.write wr (char-array stream))
+        (continue #(consumer wr %)))))]
+  (continue #(consumer writer %))))
+
 
